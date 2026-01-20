fd727d94d5632a4121a26c517636f38ac56e6d52
Ricardo <wellune@hotmail.com>
2026-01-20 12:27:03 +0100

    Día 7: Configuración JavaFx con Maven y la ventana inicial PRIORIZA

--- src/main/java/com/prioriza/MainApp.java
+++ src/main/java/com/prioriza/MainApp.java
@@ -1,7 +1,25 @@
 package com.prioriza;
 
-public class MainApp {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+    @Override
+    public void start(Stage stage){
+        Label label = new Label("PRIORIZA - App de gestión de tareas");
+        StackPane root = new StackPane(label);
+
+        Scene scene = new Scene(root, 400, 200);
+
+        stage.setTitle("PRIORIZA");
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("PRIORIZA funcionando...");
+        launch();
     }
 }
